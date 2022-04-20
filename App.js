856f1247db497907f6f8a2181fc26643abfceefa
Michael Craun <michael.craun@gmail.com>
2022-04-20 16:52:09 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello, World!</Text>
+      <Text>Well, this is kinda cool.</Text>
+      <View
+        style={{
+          height: 1,
+          backgroundColor: "black",
+          alignSelf: "stretch",
+          marginHorizontal: 5,
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignContent: "flex-start",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
